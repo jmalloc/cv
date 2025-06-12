8c382f3f9d913c2d538f6b6c34178621d7debfd6
James Harris <contact@jamesharris.id.au>
2025-06-13 08:33:48 +1000

    Use sass.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jmalloc/cv
+
+go 1.24.3
+
+require golang.org/x/sync v0.15.0
--- server.go
+++ server.go
@@ -1,9 +1,62 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
 
 func main() {
-	if err := http.ListenAndServe("0:8080", http.FileServer(http.Dir("./docs"))); err != nil {
-		panic(err)
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
+}
+
+func run() error {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		sass := exec.CommandContext(
+			ctx,
+			"sass",
+			"--watch",
+			"--style", "compressed",
+			"style.scss", "docs/style.css",
+		)
+
+		sass.Stdout = os.Stdout
+		sass.Stderr = os.Stderr
+
+		return sass.Run()
+	})
+
+	g.Go(func() error {
+		s := &http.Server{
+			Addr:    ":8080",
+			Handler: http.FileServer(http.Dir("./docs")),
+		}
+
+		go func() {
+			<-ctx.Done()
+			s.Close()
+		}()
+
+		return s.ListenAndServe()
+	})
+
+	return g.Wait()
 }
